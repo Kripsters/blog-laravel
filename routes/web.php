571bf8d1355167b5536ac13d71bf322121889fc9
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-04-25 11:25:12 +0300

    starting RESTful

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [PostController::class, "index"]);
+
+Route::get('/show/{id}', [PostController::class, "show"]);
